7aa2ca500c238bf08395ec6b85c9e44d4060ced3
Paula Cristina Engler Ribeiro <153039428+paulaengler@users.noreply.github.com>
2024-09-27 18:25:36 -0300

    commit inicial front end

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { SignOut } from "../src/contexts/SignOut";
+import { TemplatePrivado } from "./routes/privateRoute";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import CadastroUsuario from "./pages/CadastroUsuario";
+import CadastroLocais from "./pages/CadastroLocais";
+import ListagemLocais from "./pages/ListagemLocais";
+
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/cadastrousuario' element={<CadastroUsuario />} />
+            <Route path='/' element={<Home />} />
+            <Route element={<TemplatePrivado />}>
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/cadastrolocais' element={<CadastroLocais />} />
+              <Route path='/cadastrolocais/:id' element={<CadastroLocais />} />
+              <Route path='/listagemlocais' element={<ListagemLocais />} />
+              <Route path='/signout' element={<SignOut />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
