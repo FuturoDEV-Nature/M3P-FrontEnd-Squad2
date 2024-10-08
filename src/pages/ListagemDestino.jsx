38256135b1cf695d500e595c420d578aa9c7b948
Paula Cristina Engler Ribeiro <153039428+paulaengler@users.noreply.github.com>
2024-10-08 10:16:39 -0300

    correcao nas paginas dashboard, home e listagem destino, para integraçao com o back

--- src/pages/ListagemDestino.jsx
+++ src/pages/ListagemDestino.jsx
@@ -69,7 +69,7 @@ function ListagemDestino() {
           <div>
             <button
               className="btn-cadastrar-listagem"
-              onClick={() => navigate("/cadastrolocais")}
+              onClick={() => navigate("/destino/cadastrolocais")}
             >
               <MapPin size={16} /> Cadastrar{" "}
             </button>
@@ -86,7 +86,7 @@ function ListagemDestino() {
                       </td>
 
                       <td className="botao-coluna">
-                      <Link to={`/cadastrolocais/${local.destino_id}`}>
+                      <Link to={`/destino/cadastrolocais/${local.destino_id}`}>
                         <button className="btn-editar-listagem">
                           <Pencil size={16} />
                           Editar
